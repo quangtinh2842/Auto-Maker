cb7fc576da37aa94fb79fd2b78310875240d97fb
Ngô Quang Tịnh <79574292+quangtinh2842@users.noreply.github.com>
2026-03-12 14:57:19 +0700

    Delete auto_maker directory

--- auto_maker/auto_maker/REFERENCE.swift
+++
@@ -1,3 +0,0 @@
-// Temperature: https://www.waveformlighting.com/tech/calculate-color-temperature-cct-from-cie-1931-xy-coordinates
-// Greenscreening: https://developer.apple.com/documentation/coreimage/applying_a_chroma_key_effect
-
